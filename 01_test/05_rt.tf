77859ad5e720685ccf5399856f054860222859bb
zzaeha <rlaghdrud94@naver.com>
2021-11-10 17:25:29 +0900

    step1

---
+++ 01_test/05_rt.tf
@@ -0,0 +1,11 @@
+resource "aws_route_table" "zzaeha_rt" {
+  vpc_id = aws_vpc.zzaeha_vpc.id
+
+  route{
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.zzaeha_ig.id
+  }
+  tags = {
+    "Name" = "zzaeha-rt"
+  }
+}
