773b0b652a3fff59280d01628ea30bd4de2f7476
zzaeha <rlaghdrud94@naver.com>
2021-11-12 16:17:13 +0900

    step2

--- 01_test/03_subnet.tf
+++ 01_test/03_subnet.tf
@@ -1,3 +1,5 @@
+#서브넷 설정
+
 #가용영역 a의 public subnet
 resource "aws_subnet" "zzaeha_pub_a" {
   vpc_id = aws_vpc.zzaeha_vpc.id
@@ -7,6 +9,7 @@ resource "aws_subnet" "zzaeha_pub_a" {
     "Name" = "zzaeha-pub-a"
   }
 }
+
 #가용영역 a의 private subnet
 resource "aws_subnet" "zzaeha_pri_a" {
   vpc_id = aws_vpc.zzaeha_vpc.id
@@ -16,6 +19,7 @@ resource "aws_subnet" "zzaeha_pri_a" {
     "Name" = "zzaeha-pri-a"
   }
 }
+
 #가용영역 c의 public subnet
 resource "aws_subnet" "zzaeha_pub_c" {
   vpc_id = aws_vpc.zzaeha_vpc.id
@@ -25,6 +29,7 @@ resource "aws_subnet" "zzaeha_pub_c" {
     "Name" = "zzaeha-pub-c"
   }
 }
+
 #가용영역 c의 private subnet
 resource "aws_subnet" "zzaeha_pri_c" {
   vpc_id = aws_vpc.zzaeha_vpc.id
@@ -32,5 +37,26 @@ resource "aws_subnet" "zzaeha_pri_c" {
   availability_zone = "ap-northeast-2c"
   tags = {
     "Name" = "zzaeha-pri-c"
+  }
+}
+
+
+#가용영역 a의 private DB subnet
+resource "aws_subnet" "zzaeha_pri_dba" {
+  vpc_id = aws_vpc.zzaeha_vpc.id
+  cidr_block = "10.0.4.0/24"
+  availability_zone = "ap-northeast-2a"
+  tags = {
+    "Name" = "zzaeha-pri-dba"
+  }
+}
+
+#가용영역 c의 private DB subnet
+resource "aws_subnet" "zzaeha_pri_dbc" {
+  vpc_id = aws_vpc.zzaeha_vpc.id
+  cidr_block = "10.0.5.0/24"
+  availability_zone = "ap-northeast-2c"
+  tags = {
+    "Name" = "zzaeha-pri-dbc"
   }
 }
