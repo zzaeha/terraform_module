0f96c237d9289c136b198a3c0a1d7bb2339d5b77
zzaeha <rlaghdrud94@naver.com>
2021-11-12 14:21:49 +0900

    step1

--- 01_test/15_albtgatt.tf
+++ 01_test/15_albtgatt.tf
@@ -1,4 +1,4 @@
-#ALB Target group Attachement 지정
+#ALB Target group Attachment 지정
 resource "aws_lb_target_group_attachment" "zzaeha_tgatt" {
   target_group_arn = aws_lb_target_group.zzaeha_albtg.arn
   target_id = aws_instance.zzaeha_web_a.id
