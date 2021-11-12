ca55c5d6b984b4314e5049ccb1bacdffc935f69a
zzaeha <rlaghdrud94@naver.com>
2021-11-12 11:42:24 +0900

    step1

---
+++ 01_test/12_alb.tf
@@ -0,0 +1,12 @@
+#Application Load Balancer 생성
+resource "aws_lb" "zzaeha_alb" {
+  name = "zzaeha-alb"
+  internal = false
+  load_balancer_type = "application"
+  security_groups = [aws_security_group.zzaeha_sg.id]
+  subnets = [aws_subnet.zzaeha_pub_a.id, aws_subnet.zzaeha_pub_c.id]
+
+  tags = {
+    "Name" = "zzaeha-alb"
+  }
+}
