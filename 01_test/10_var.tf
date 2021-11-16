1c4ee5234bacb1f7bb8d23e0de97ef8f148b1157
zzaeha <rlaghdrud94@naver.com>
2021-11-16 15:27:19 +0900

    final

--- 01_test/10_var.tf
+++ 01_test/10_var.tf
@@ -30,150 +30,150 @@ variable "cidr" {
 
 variable "cidr_v6" {
   type = string
-    default = "::/0"
+  #  default = "::/0"
 }
 
 variable "cidr_public" {
   type = list(string)
-    default = [ "10.0.0.0/24", "10.0.2.0/24" ]
+  #  default = [ "10.0.0.0/24", "10.0.2.0/24" ]
 }
 
 variable "cidr_private" {
   type = list(string)
-    default = [ "10.0.1.0/24", "10.0.3.0/24" ]
+  #  default = [ "10.0.1.0/24", "10.0.3.0/24" ]
 }
 
 variable "cidr_privatedb" {
   type = list(string)
-    default = [ "10.0.4.0/24", "10.0.5.0/24"]
+  #  default = [ "10.0.4.0/24", "10.0.5.0/24"]
 }
 
 variable "pri_ip" {
   type = string
-    default = "10.0.0.11"
+  #  default = "10.0.0.11"
 }
 
 variable "protocol_tcp" {
   type  = string
-   default = "tcp"
+  # default = "tcp"
 }
 
 variable "protocol_udp" {
   type  = string
-   default = "udp"
+  # default = "udp"
 }
 
 variable "protocol_http" {
   type  = string
-   default = "http"
+  # default = "http"
 }
 
 variable "protocol_http1" {
   type  = string
-   default = "HTTP"
+  # default = "HTTP"
 }
 
 variable "protocol_icmp" {
   type  = string
-   default = "icmp"
+  # default = "icmp"
 }
 
 variable "protocol_ssh" {
   type  = string
-   default = "ssh"
+  # default = "ssh"
 }
 
 variable "port_ssh" {
   type  = number
-   default = 22
+  # default = 22
 }
 
 variable "port_http" {
   type  = number
-   default = 80
+  # default = 80
 }
 
 variable "port_mysql" {
   type  = number
-   default = 3306
+  # default = 3306
 }
 
 variable "port_zero" {
   type = number
-   default = 0
+  # default = 0
 }
 
 variable "port_minus" {
   type = number
-   default = -1
+  # default = -1
 }
 
 variable "protocol_minus" {
   type  = number
-   default = -1
+  # default = -1
 }
 
 variable "db_name" {
   type  = string
-   default = "mysql"
+  # default = "mysql"
 }
 
 variable "load_type" {
   type  = string
-   default = "application"
+  # default = "application"
 }
 
 variable "strategy" {
   type  = string
-   default = "cluster"
+  # default = "cluster"
 }
 
 variable "storage_size" {
   type = number
-   default = 20
+  # default = 20
 }
 
 variable "storage_type" {
   type = string
-   default = "gp2"
+  # default = "gp2"
 }
 
 variable "mysql_version" {
   type = string
-   default = "8.0"
+  # default = "8.0"
 }
 
 variable "sql_engine" {
   type = string
-   default = "mysql"
+  # default = "mysql"
 }
 
 variable "username" {
   type = string
-   default = "admin"
+  # default = "admin"
 }
 
 variable "password" {
   type = string
-   default = "It12345!"
+  # default = "It12345!"
 }
 
 variable "name_db" {
   type  = string
-   default = "mydb"
+  # default = "mydb"
 }
 
 variable "instance" {
   type = string
-  default = "t2.micro"
+ # default = "t2.micro"
 }
 
 variable "instance_db" {
   type  = string
-   default = "db.t2.micro"
+  # default = "db.t2.micro"
 }
 
 variable "endpoint" {
   type  = string
-  default = "data.aws_db_instance.mydb.endpoint"
+ # default = "data.aws_db_instance.mydb.endpoint"
 }
